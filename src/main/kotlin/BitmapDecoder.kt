929d666549882b1b4b82da56d6786ed5b2663840
Pavel Vasiliev <pavel.v96@yandex.ru>
2018-06-23 18:18:17 +0300

    close source after full read

--- src/main/kotlin/BitmapDecoder.kt
+++ src/main/kotlin/BitmapDecoder.kt
@@ -8,36 +8,38 @@ import java.io.File
 
 fun decode(filePath: String): Bitmap {
     val source = Okio.buffer(Okio.source(File(filePath)))
-    val signature = source.readUtf8(2)
-    val size = source.readIntLe()
-    val reserved = source.readIntLe()
-    val dataOffset = source.readIntLe()
-    val fileHeader = FileHeader(signature, size, reserved, dataOffset)
-    val headerSize = source.readIntLe()
-    val width = source.readIntLe()
-    val height = source.readIntLe()
-    val planes = source.readShortLe()
-    val bitsPerPixel = source.readShortLe()
-    val compression = source.readIntLe()
-    val imageSize = source.readIntLe()
-    val xPixelsPerMeter = source.readIntLe()
-    val yPixelsPerMeter = source.readIntLe()
-    val numUsedColors = source.readIntLe()
-    val numImportantColors = source.readIntLe()
-    val imageHeader = ImageHeader(headerSize, width, height, planes, bitsPerPixel, compression,
-            imageSize, xPixelsPerMeter, yPixelsPerMeter, numUsedColors, numImportantColors)
-    val colorTable = (0 until numUsedColors).map {
-        val b = source.readByteString(1).hex().toInt(16)
-        val g = source.readByteString(1).hex().toInt(16)
-        val r = source.readByteString(1).hex().toInt(16)
-        source.skip(1)
-        Color(r, g, b)
+    return source.use {
+        val signature = readUtf8(2)
+        val size = readIntLe()
+        val reserved = readIntLe()
+        val dataOffset = readIntLe()
+        val fileHeader = FileHeader(signature, size, reserved, dataOffset)
+        val headerSize = readIntLe()
+        val width = readIntLe()
+        val height = readIntLe()
+        val planes = readShortLe()
+        val bitsPerPixel = readShortLe()
+        val compression = readIntLe()
+        val imageSize = readIntLe()
+        val xPixelsPerMeter = readIntLe()
+        val yPixelsPerMeter = readIntLe()
+        val numUsedColors = readIntLe()
+        val numImportantColors = readIntLe()
+        val imageHeader = ImageHeader(headerSize, width, height, planes, bitsPerPixel, compression,
+                imageSize, xPixelsPerMeter, yPixelsPerMeter, numUsedColors, numImportantColors)
+        val colorTable = (0 until numUsedColors).map {
+            val b = readByteString(1).hex().toInt(16)
+            val g = readByteString(1).hex().toInt(16)
+            val r = readByteString(1).hex().toInt(16)
+            skip(1)
+            Color(r, g, b)
+        }
+        val pixelData = (0 until imageSize).map {
+            val colorIndex = readByteString(1).hex().toInt(16)
+            colorTable[colorIndex]
+        }
+        Bitmap(fileHeader, imageHeader, colorTable, pixelData)
     }
-    val pixelData = (0 until imageSize).map {
-        val colorIndex = source.readByteString(1).hex().toInt(16)
-        colorTable[colorIndex]
-    }
-    return Bitmap(fileHeader, imageHeader, colorTable, pixelData)
 }
 
 fun Bitmap.asBufferedImage(): BufferedImage {
@@ -53,4 +55,10 @@ fun Bitmap.asBufferedImage(): BufferedImage {
     val transform = AffineTransform.getRotateInstance(Math.toRadians(180.0), width / 2.0, height / 2.0)
     val op = AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR)
     return op.filter(bufferedImage, null)
+}
+
+fun <T : AutoCloseable, R> T.use(block: T.() -> R): R {
+    val result = block()
+    close()
+    return result
 }
